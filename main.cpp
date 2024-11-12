ac2b82b9c3e74b353db628982239ceb9b1cabb3c
Zinash43 <zinashgetiso@gmail.com>
2024-11-12 05:52:07 -0900

    Update main.cpp
    int SetDifficulty()

--- main.cpp
+++ main.cpp
@@ -32,6 +32,32 @@ snakesDirection sDir;
 // boolean variable for checking game is over or not
 bool isGameOver;
 
+
+// Function to set the game difficulty level
+int SetDifficulty()
+{
+    int dfc, choice;
+    cout << "\nSET DIFFICULTY\n1: Easy\n2: Medium\n3: hard "
+            "\nNOTE: if not chosen or pressed any other "
+            "key, the difficulty will be automatically set "
+            "to medium\nChoose difficulty level: ";
+    cin >> choice;
+    switch (choice) {
+    case '1':
+        dfc = 50;
+        break;
+    case '2':
+        dfc = 100;
+        break;
+    case '3':
+        dfc = 150;
+        break;
+    default:
+        dfc = 100;
+    }
+    return dfc;
+}
+
 // Function to initialize game variables
 void GameInit()
 {
